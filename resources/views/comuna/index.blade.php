1d7bdf246ae4894acbecb8f06bcd987b55259329
Juan Muñoz <trabajosfcecep4@gmail.com>
2025-03-27 23:41:53 -0500

    Commune Edit y correcion de errores

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -32,12 +32,15 @@
                         <td>{{ $comuna->comu_nomb }}</td>
                         <td>{{ $comuna->muni_nomb }}</td>
                         <td>
-                            <form action ="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}"
-                                method='POST' style="display: inline-block">
-                                @method('delete')
-                                @csrf
-                                <input class="btn btn-danger" type="submit" value="Delete">
-                            </form>
+                            <a href="{{route('comunas.edit',['comuna'=>$comuna->comu_codi]) }}"
+                                class="btn btn-info">Edit</a></li>
+
+                                <form action="{{route('comunas.destroy',['comuna' => $comuna->comu_codi])}}"
+                             method="POST" style="display: inline-block">
+                             @method('delete')
+                             @csrf
+                             <input type="submit" class="btn btn-danger" value="Delete">
+                           </form>
                         </td>
                     </tr>
                 @endforeach
